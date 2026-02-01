99ddd9c021099850a44041c6820ea23cc965d64e
Nur Rabby Shuvo <106588083+0xnurrabby@users.noreply.github.com>
2026-02-01 16:24:53 +0600

    Delete src directory

--- src/lib/builderCode.ts
+++
@@ -1,7 +0,0 @@
-import { Attribution } from 'https://esm.sh/ox/erc8021'
-
-export const BUILDER_CODE = "TODO_REPLACE_BUILDER_CODE"
-
-export const dataSuffix = Attribution.toDataSuffix({
-  codes: [BUILDER_CODE],
-})
